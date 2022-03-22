55fd5492a0e87e8e170434484790aeddd5301de9
Hans <hmtoquicac@unal.edu.co>
2022-03-21 20:24:13 -0500

    feat(global): add health page, improve code distribution

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const resolvers = require('./resolvers')
-const types = require('./schema')
+const resolvers = require('./src/resolvers')
+const types = require('./src/schema')
 const { buildSchema } = require('graphql')
 
 const app = express()
-app.use('/graphql', graphqlHTTP({
+app.get('/health', (req, res) =>
+  res.status(200).send({
+    message: 'Welcome to the DTF-API'
+  })
+)
+app.use('/', graphqlHTTP({
   schema: buildSchema(types),
   rootValue: resolvers,
   graphiql: true
 }))
 
 app.listen(4000)
-console.log('Running a GraphQL API server at localhost:4000/graphql')
+console.log('Running a GraphQL API server at localhost:4000')
